fbdb6bb163cd3788b98b189496967c144058cecf
Divyachelikani <123126195+Divyachelikani@users.noreply.github.com>
2023-11-06 12:16:29 -0600

    Implemented java code for CustomerSupport.

--- SaveTheStarve2/app/src/main/java/com/example/savethestarve/CustomerSupport.java
+++ SaveTheStarve2/app/src/main/java/com/example/savethestarve/CustomerSupport.java
@@ -1,13 +1,28 @@
 package com.example.savethestarve;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class CustomerSupport extends MainActivity{
+public class CustomerSupport extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private EditText emailEditText;
+    private EditText messageEditText;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer);
-    }
-}
 
+        nameEditText = findViewById(R.id.name);
+        emailEditText = findViewById(R.id.email);
+        messageEditText = findViewById(R.id.message);
+        submitButton = findViewById(R.id.submit);
+
+    }
+
+}
