92bd28a14f7c2f754d18bffcded148cf864d3099
Neeha-Beerkur <123040403+Neeha-Beerkur@users.noreply.github.com>
2023-11-08 17:21:26 -0600

    Implemented the java code for receive activity

--- SaveTheStarve2/app/src/main/java/com/example/savethestarve/RecieveActivity.java
+++ SaveTheStarve2/app/src/main/java/com/example/savethestarve/RecieveActivity.java
@@ -4,9 +4,33 @@ import android.os.Bundle;
 
 public class RecieveActivity extends MainActivity {
 
+    private EditText NameET;
+   private EditText AddressET;
+   private Button submitBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recieve);
+
+
+
+        NameET = findViewById(R.id.NameET);
+        AddressET = findViewById(R.id.AddressET);
+        submitBtn = findViewById(R.id.submitBtn);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Name = NameET.getText().toString();
+                String Address = AddressET.getText().toString();
+
+                String message = "Name: " + Name + "\n Address: " + Address;
+                Toast.makeText(RecieveActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
+
+    
 }
