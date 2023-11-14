0ad6ead4b03872719288d9e6fa061c8046013999
Sai Nandini Devabattula <123043897+Nandini-Devabattula@users.noreply.github.com>
2023-11-13 22:03:27 -0600

    implemented the list functionality for donate activity and navigated to reciever activity with the given input values from the user.

--- SaveTheStarve2/app/src/main/java/com/example/savethestarve/DonateActivity.java
+++ SaveTheStarve2/app/src/main/java/com/example/savethestarve/DonateActivity.java
@@ -1,11 +1,15 @@
 package com.example.savethestarve;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DonateActivity extends MainActivity {
 
@@ -14,16 +18,19 @@ public class DonateActivity extends MainActivity {
     EditText description;
     EditText phoneNumber;
     Button submit;
+    List<String> userDetailsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donate);
-        try{
+        try {
             name = findViewById(R.id.editTextName);
             address = findViewById(R.id.editTextAddress);
             description = findViewById(R.id.editTextDescription);
             phoneNumber = findViewById(R.id.editTextPhone);
             submit = findViewById(R.id.buttonSubmit);
+            userDetailsList = new ArrayList<>();
 
             submit.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -33,18 +40,41 @@ public class DonateActivity extends MainActivity {
                     String userDescription = description.getText().toString();
                     String userMobile = phoneNumber.getText().toString();
 
-                    if(userName.isEmpty() || userAddress.isEmpty() || userDescription.isEmpty() || userMobile.isEmpty()){
+                    if (userName.isEmpty() || userAddress.isEmpty() || userDescription.isEmpty() || userMobile.isEmpty()) {
                         Toast.makeText(getApplicationContext(), "Enter all inputs", Toast.LENGTH_LONG).show();
-                    }else{
+                    } else {
+                        // Create a string to represent the user details
+                        String userDetails = "Name: " + userName +
+                                "\nAddress: " + userAddress +
+                                "\nDescription: " + userDescription +
+                                "\nPhone Number: " + userMobile;
+
+                        // Add the user details to the list
+                        userDetailsList.add(userDetails);
+
+                        // Create an Intent to start the ReceiverActivity
+                        Intent intent = new Intent(DonateActivity.this, RecieveActivity.class);
+
+                        // Pass the list to the ReceiverActivity
+                        intent.putStringArrayListExtra("USER_DETAILS_LIST", (ArrayList<String>) userDetailsList);
+
+                        // Start the ReceiverActivity with the Intent
+                        startActivity(intent);
+
+                        // Clear the EditText fields
                         name.setText("");
                         address.setText("");
                         description.setText("");
                         phoneNumber.setText("");
-                        Toast.makeText(getApplicationContext(),"Details submitted successfully", Toast.LENGTH_LONG).show();
+
+                        Toast.makeText(getApplicationContext(), "Details submitted successfully", Toast.LENGTH_LONG).show();
                     }
                 }
             });
-        }catch(Exception e){
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
 
         }
     }
--- SaveTheStarve2/app/src/main/java/com/example/savethestarve/RecieveActivity.java
+++ SaveTheStarve2/app/src/main/java/com/example/savethestarve/RecieveActivity.java
@@ -1,6 +1,10 @@
 package com.example.savethestarve;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class RecieveActivity extends MainActivity {
 
@@ -8,5 +12,24 @@ public class RecieveActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recieve);
+
+        TextView userDetailsTextView = findViewById(R.id.userDetailsTextView);
+
+        // Retrieve the list from the Intent
+        Intent intent = getIntent();
+        List<String> userDetailsList = intent.getStringArrayListExtra("USER_DETAILS_LIST");
+
+        // Display the list in the TextView
+        StringBuilder userDetailsStringBuilder = new StringBuilder();
+        for (String userDetails : userDetailsList) {
+            userDetailsStringBuilder.append(userDetails).append("\n\n");
+        }
+        userDetailsTextView.setText(userDetailsStringBuilder.toString());
     }
 }
+
+
+
+
+
+
