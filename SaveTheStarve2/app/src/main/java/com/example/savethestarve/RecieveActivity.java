0958eef7e1f1d6af8db3c159c9a10697edc0d692
Divyachelikani <123126195+Divyachelikani@users.noreply.github.com>
2023-11-13 20:06:15 -0600

    modified xml files

--- SaveTheStarve2/app/src/main/java/com/example/savethestarve/RecieveActivity.java
+++ SaveTheStarve2/app/src/main/java/com/example/savethestarve/RecieveActivity.java
@@ -1,6 +1,10 @@
 package com.example.savethestarve;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RecieveActivity extends MainActivity {
 
