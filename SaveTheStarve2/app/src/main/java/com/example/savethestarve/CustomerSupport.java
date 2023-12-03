e1191b72bc1dfb8722d59a20799326f2f51ed9d0
Neeha-Beerkur <123040403+Neeha-Beerkur@users.noreply.github.com>
2023-12-02 23:55:24 -0600

    Modified customer support

--- SaveTheStarve2/app/src/main/java/com/example/savethestarve/CustomerSupport.java
+++ SaveTheStarve2/app/src/main/java/com/example/savethestarve/CustomerSupport.java
@@ -23,6 +23,32 @@ public class CustomerSupport extends AppCompatActivity {
         messageEditText = findViewById(R.id.message);
         submitButton = findViewById(R.id.submit);
 
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String message = messageEditText.getText().toString();
+
+                if (!name.isEmpty() && !email.isEmpty() && !message.isEmpty()) {
+                    // Perform your submission logic here
+
+                    // Display a toast message
+                    String toastMessage = "Details Submitted";
+                    showToast(toastMessage);
+
+                    // Clear EditText fields after submission
+                    nameEditText.getText().clear();
+                    emailEditText.getText().clear();
+                    messageEditText.getText().clear();
+                } else {
+                    showToast("Please fill in all the details.");
+                }
+            }
+        });
     }
 
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 }
